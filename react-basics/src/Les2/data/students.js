86d14f511ef0e37a0e4af4a6add5bfa5899de7b3
Michael Vanderpoorten <michael@yungpanda.com>
2022-02-24 16:36:29 +0100

    ✨ Add students list

---
+++ react-basics/src/Les2/data/students.js
@@ -0,0 +1,231 @@
+const students = [
+  {
+    name: "Bauwens Milan",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Beirnaert Diederik",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Bien Patricia",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Bodyn Tiele",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Cherchié Thibaud",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Claeys Vincent",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Coppens d'Eeckenbrugge Menno",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "De Baere Andries",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "De Boever Lisa",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "De Brauwer Wout",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "De Buck Elooi",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Deneckere Robin",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Duffeleer Niels",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Dupont Wouter",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Feraux Thibault",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Kaya Dilara",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Lannoy Alexander",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+  {
+    name: "Maenhout Iris",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Minne Niels",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Pieters Rien",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Reinhard Michael",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Schulte Naboth",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Sinnaeve Jorne",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Spaan Ruben",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Tack Wouter",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Van Cuyck Keano",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Van der Donckt Rossi",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Van Hoorde Bjorn",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Van Rossem Kyan",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Vandamme Dylan",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Vandenheede Bent",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Verheecke Lorenzo",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+
+  {
+    name: "Verniers Stef",
+    image:
+      "https://arteveldehogeschool.instructure.com/images/messages/avatar-50.png",
+    username: "",
+  },
+];

7990b061ef70c8511c29613ecaf77d8e13d06411
Michael Vanderpoorten <michael@yungpanda.com>
2022-03-01 13:19:45 +0100

    ✨ Les 2

--- react-basics/src/Les2/Examples/GentseFeesten/GentseFeesten.js
+++ react-basics/src/Les2/Examples/GentseFeesten/GentseFeesten.js
@@ -1,3 +1,27 @@
-const GentseFeesten = () => {};
+import { useEffect, useState } from "react";
+
+const GentseFeesten = () => {
+  const [events, setEvents] = useState();
+
+  useEffect(() => {
+    fetch("https://www.pgm.gent/data/gentsefeesten/events_500.json")
+      .then((data) => data.json())
+      .then((events) => setEvents(events))
+      .catch((error) => {
+        // TODO
+      });
+  }, []);
+
+  if (!events) {
+    return <p>Loading</p>;
+  }
+  return (
+    <div>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
 
 export default GentseFeesten;

d9107a13292254f1d581f34fd0b5f2646a059708
Michael Vanderpoorten <michael@yungpanda.com>
2022-05-04 10:32:09 +0200

    🐛 Fix imports

--- froooty/app/src/components/App/Auth/AuthContainer.js
+++ froooty/app/src/components/App/Auth/AuthContainer.js
@@ -1,6 +1,6 @@
 import { Navigate, useLocation } from "react-router-dom";
-import { AuthRoutes } from "../../core/routing";
-import { useAuthContext } from "./Auth/AuthProvider";
+import { AuthRoutes } from "../../../core/routing";
+import { useAuthContext } from "./AuthProvider";
 
 const AuthContainer = ({ children }) => {
     const { auth } = useAuthContext();

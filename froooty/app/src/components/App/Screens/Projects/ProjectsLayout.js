95faca07915e9e6d59027952370806ca867d095f
Michael Vanderpoorten <michael@yungpanda.com>
2022-05-20 12:52:02 +0200

    🚧 Preps Les 11

--- froooty/app/src/components/App/Screens/Projects/ProjectsLayout.js
+++ froooty/app/src/components/App/Screens/Projects/ProjectsLayout.js
@@ -1,11 +1,6 @@
-import { useTranslation } from "react-i18next";
 import { Outlet } from "react-router-dom";
-import useTitle from "../../../../core/hooks/useTitle";
 
 const ProjectsLayout = () => {
-    const { t } = useTranslation();
-    useTitle(t("projects.title"));
-
     return (
         <div>
             <Outlet />
